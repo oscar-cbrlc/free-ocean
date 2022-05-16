f687556e669e84db13847a70c75b21c5f9e3b829
OscarCabralesA <oscar.cbrlc@gmail.com>
2022-05-15 20:32:01 -0500

    Update app.js, port assignment

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const cors = require('cors')
 const Meeting = require("./lib/models/Meeting");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Add Access Control Allow Origin headers
 /*app.use((req, res, next) => {
