cee02d35f3a89c6398821f0b64dea7fe09be4079
OscarCabralesA <oscar.cbrlc@gmail.com>
2022-05-15 20:29:30 -0500

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,18 @@ const Meeting = require("./lib/models/Meeting");
 const app = express();
 const port = 3000;
 
+// Add Access Control Allow Origin headers
+/*app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "https://yoursite.com");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res
+    next();
+  });*/
+
+// initial path in localhost:3000
 app.get('/', (req, res) => {
     res.status(200).send('API for FreeOcean meetings!')
 })
