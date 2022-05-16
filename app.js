d2709784bf824f850dda78ee26de4c9bea1b67aa
OscarCabralesA <oscar.cbrlc@gmail.com>
2022-05-15 23:17:37 -0500

    Fix in endpoints

--- app.js
+++ app.js
@@ -3,19 +3,21 @@ const MeetingsController = require("./lib/controllers/MeetingsController");
 const express = require("express");
 const cors = require('cors')
 const Meeting = require("./lib/models/Meeting");
+const { json } = require("express/lib/response");
 const app = express();
 const port = process.env.PORT || 3000;
 
+
+app.use(express.json());
 // Add Access Control Allow Origin headers
-/*app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://yoursite.com");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    res
+app.use(function(req, res, next) {
+    
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
     next();
-  });*/
+});
 
 // initial path in localhost:3000
 app.get('/', (req, res) => {
@@ -30,11 +32,11 @@ app.get('/v1/meetings', cors(), (req, res) => {
 
 // returns an object 
 // http://localhost:3000/explorersInNode
-app.post('/v1/name=:name&&org=:org&&desc=:desc&&tags=:tags', cors(), (req, res) => {
-    const name = req.params.name;
-    const org = req.params.org;
-    const desc = req.params.desc;
-    const tags = req.params.tags;
+app.post('/v1/meetings/add', (req, res) => {
+    const name = req.body.name;
+    const org = req.body.org;
+    const desc = req.body.desc;
+    const tags = req.body.tags;
     const meeting = new Meeting(name, org, desc, tags, new Date(), new Date());
 
 
